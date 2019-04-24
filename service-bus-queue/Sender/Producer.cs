e5069dc56aa2541a8755fd3e2840283384662442
Manuel <manuel@blaucompany.co>
2019-04-24 16:37:10 -0500

    Service Bus sender.

---
+++ service-bus-queue/Sender/Producer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+
+namespace Sender
+{
+
+    class Producer {
+
+        private IQueueClient client;
+
+        public Producer() {
+            client = new QueueClient(Configuration.Instance.ConnectionString, 
+                                                    Configuration.Instance.QueueName);
+        }
+
+        public async Task SendMessage(object messageObject) {
+            string messageString = JsonConvert.SerializeObject(messageObject);
+            await SendMessage(messageString);
+        }
+
+        public async Task SendMessage(string message) {
+            Console.WriteLine("Message to send: " + message);
+            byte[] messageArray = Encoding.UTF8.GetBytes(message);
+            var messageObject = new Message(messageArray);
+
+            await client.SendAsync(messageObject);
+
+        }
+
+        public async Task Close() {
+            await client.CloseAsync();
+        }
+
+    }
+
+}
---
+++ service-bus-queue/Sender/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+using Sender.Entities;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Producer producer = new Producer();
+
+            int command = 1;
+            while(command == 1) {
+
+                var message = new Message {
+                    MessageId = Guid.NewGuid(),
+                    CreatedAt = DateTime.Now,
+                    Payload = new Order {
+                        OrderId = Guid.NewGuid(),
+                        ProductName = "Scooter",
+                        Price = 299
+                    }
+                };
+
+                Console.WriteLine("Sending order with ID " + message.Payload.OrderId);
+
+                producer.SendMessage(message).GetAwaiter().GetResult();
+                
+                Console.WriteLine("Message sent");
+
+                Console.WriteLine("Do you want to send a new order? \n1. Yes \n2. No");
+                command = Convert.ToInt32(Console.ReadLine());
+            }
+
+            producer.Close().GetAwaiter().GetResult();
+
+            Console.WriteLine("Done.");
+
+        }
+    }
+}
